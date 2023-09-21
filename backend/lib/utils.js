486f54dc041065e90ddcf6a8a4c65072728402ce
Alejandro <kalep99@gmail.com>
2023-09-21 19:08:46 +0200

    feat: Add token generation utility in utils

---
+++ backend/lib/utils.js
@@ -0,0 +1,16 @@
+//aquí verificamos los token (middleware de autenticación)
+
+const jwt = require("jsonwebtoken");
+
+//genere un token u otro dependiendo si es de refresco o no.
+const generateTokens = (user, isRefreshToken) => {
+  if (isRefreshToken) {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "20m",
+    });
+  } else {
+    return jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: "15m" });
+  }
+};
+
+module.exports = { generateTokens };
