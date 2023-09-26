8c9c1ff6996cfc90387b7bdbda8b178b482e89f5
Alejandro <kalep99@gmail.com>
2023-09-26 17:44:29 +0200

    style: Adding new styles

--- frontend/src/utils/Navigation/Navbar.js
+++ frontend/src/utils/Navigation/Navbar.js
@@ -1,39 +1,62 @@
 import { NavLink } from "react-router-dom";
 import "../Navigation/Navbar.css";
+import { useState, useEffect } from "react";
 
 function Navbar() {
+  //para el sticky navbar
+  const [scrolled, setScrolled] = useState(false);
+
+  //manejar el sticky navbar
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            className="nav-link" // Agregar la clase nav-link aquí
-            activeclassname="active" // Opcional, si deseas resaltar el enlace activo
-            to="/index/Page/1.html"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="nav-link" // Agregar la clase nav-link aquí
-            activeclassname="active" // Opcional, si deseas resaltar el enlace activo
-            to="/Login"
-          >
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            className="nav-link" // Agregar la clase nav-link aquí
-            activeclassname="active" // Opcional, si deseas resaltar el enlace activo
-            to="/User"
-          >
-            User
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+    <header className={scrolled ? "scrolled" : "scrolledoff"}>
+      <nav>
+        <h1>Index Search</h1>
+        <ul>
+          <li>
+            <NavLink
+              className="nav-link" // Agregar la clase nav-link aquí
+              activeclassname="active" // Opcional, si deseas resaltar el enlace activo
+              to="/index/Page/1.html"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="nav-link" // Agregar la clase nav-link aquí
+              activeclassname="active" // Opcional, si deseas resaltar el enlace activo
+              to="/Login"
+            >
+              Login
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              className="nav-link" // Agregar la clase nav-link aquí
+              activeclassname="active" // Opcional, si deseas resaltar el enlace activo
+              to="/User"
+            >
+              User
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
 
--- frontend/src/views/Search/HomeSearch.js
+++ frontend/src/views/Search/HomeSearch.js
@@ -127,10 +127,30 @@ function Index() {
         <StayLogged visible={visible} onStayLoggedIn={handleRefreshToken} />,
         document.querySelector("#modal")
       )}*/}
-      <header>
-        <h1>Index Search</h1>
-        <Navbar></Navbar>
-      </header>
+
+      <Navbar></Navbar>
+
+      <section>
+        <div className={classes.images}>
+          <img
+            className={classes.backimages}
+            src="/images/pixabay.jpg"
+            alt="Mi Imagen"
+          />
+          <div className={classes.thinContainer}>
+            <h1> Fanfiction searcher</h1>
+            <h2>
+              "El fanfiction es una forma de amor y gratitud hacia las historias
+              y los personajes que más nos han impactado. Es una celebración de
+              la imaginación y la creatividad, y una demostración del poder
+              duradero de una buena historia para inspirar a otros a soñar y
+              crear." ~ Rainbow Rowell
+            </h2>
+          </div>
+        </div>
+      </section>
+
+      <div className={classes.line}></div>
 
       {/*end of navbar */}
       <div className={classes.Container}>
